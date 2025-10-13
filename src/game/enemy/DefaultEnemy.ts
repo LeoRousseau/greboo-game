9cf81c03ec5dbf1d6617cca760926611a97434f6
LeoRousseau <apb.leorousseau@gmail.com>
2025-10-13 20:23:58 +0200

    add ghost sprite

--- src/game/enemy/DefaultEnemy.ts
+++ src/game/enemy/DefaultEnemy.ts
@@ -1,15 +1,18 @@
-import { Container, Graphics } from "pixi.js";
+import { AnimatedSprite, Assets, Container } from "pixi.js";
 import type { IPoint } from "../types/IPoint";
 import Matter from "matter-js";
 import type { Engine } from "../engine/Engine";
 
 export class DefaultEnemy {
   readonly points: [IPoint, IPoint];
-  readonly sprite: Graphics;
+  sprite?: AnimatedSprite;
   readonly body: Matter.Body;
   facingPointIndex = 1;
 
-  speed: number = 0.6;
+  speed: number = 0.4;
+
+  private _container?: Container;
+  private _index?: number;
 
   constructor(
     readonly engine: Engine,
@@ -17,10 +20,19 @@ export class DefaultEnemy {
   ) {
     this.points = points;
 
-    this.sprite = new Graphics();
-    this.sprite.beginFill(0xff0000);
-    this.sprite.drawEllipse(0, 0, 15, 20);
-    this.sprite.endFill();
+    Assets.load("ghost_spritesheet.json").then((sheet) => {
+      console.log(sheet);
+      this.sprite = new AnimatedSprite(sheet.animations.walk);
+      this.sprite.animationSpeed = 0.15;
+      this.sprite.play();
+      this.sprite.anchor.set(0.5);
+      this.sprite.x = points[0].x;
+      this.sprite.y = points[0].y;
+
+      if (this._container && this._index) {
+        this._container.addChildAt(this.sprite, this._index);
+      }
+    });
 
     this.body = Matter.Bodies.rectangle(points[0].x, points[0].y, 25, 40, {
       restitution: 0,
@@ -32,10 +44,14 @@ export class DefaultEnemy {
   }
 
   addTo(container: Container, index = 0) {
-    container.addChildAt(this.sprite, index);
+    this._container = container;
+    this._index = index;
+    if (this.sprite) container.addChildAt(this.sprite, index);
   }
 
   update() {
+    if (!this.sprite) return;
+
     const target = this.points[this.facingPointIndex];
     const distance = target.x - this.body.position.x;
     if (Math.abs(distance) < 5) {
@@ -46,7 +62,10 @@ export class DefaultEnemy {
       x: Math.sign(target.x - this.body.position.x) * this.speed,
       y: this.body.velocity.y,
     });
+
     this.sprite.x = this.body.position.x;
     this.sprite.y = this.body.position.y;
+
+    this.sprite.scale.x = this.body.velocity.x > 0 ? 1 : -1;
   }
 }
